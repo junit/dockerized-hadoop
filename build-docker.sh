7c52e5ce867c6e100a9458609a705f7ef7f68830
Lu Yiwen <luyiwen@tuso.net>
2023-05-05 07:00:47 +0800

    upgrade version to 3.2.4

--- build-docker.sh
+++ build-docker.sh
@@ -2,9 +2,9 @@
 
 VERSION=3.2.4
 
-docker buildx build --progress plain --build-arg VERSION=$VERSION  --target hadoop-base -t junit/hadoop-base:$VERSION hadoop-base
-docker buildx build --progress plain --build-arg VERSION=$VERSION -t junit/hdfs-namenode:$VERSION hdfs-namenode
-docker buildx build --progress plain --build-arg VERSION=$VERSION -t junit/hdfs-datanode:$VERSION hdfs-datanode
+docker build --build-arg version=$VERSION -t junit/hadoop-base:$VERSION hadoop-base
+docker build --build-arg version=$VERSION -t junit/hdfs-namenode:$VERSION hdfs-namenode
+docker build --build-arg version=$VERSION -t junit/hdfs-datanode:$VERSION hdfs-datanode
 
 docker tag junit/hadoop-base:$VERSION junit/hadoop-base:latest
 docker tag junit/hdfs-namenode:$VERSION junit/hdfs-namenode:latest
